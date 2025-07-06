d037cc7c1d727bbe62f772c07cab3be2b26e2ca4
terrymanzi <m.terry@alustudent.com>
2025-07-06 03:27:37 +0200

    auth_bloc

---
+++ lib/blocs/auth/auth_bloc.dart
@@ -0,0 +1,67 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../repositories/auth_repository.dart';
+import 'auth_event.dart';
+import 'auth_state.dart';
+
+class AuthBloc extends Bloc<AuthEvent, AuthState> {
+  final AuthRepository _authRepository;
+
+  AuthBloc({required AuthRepository authRepository})
+    : _authRepository = authRepository,
+      super(AuthInitial()) {
+    on<AuthSignUpRequested>(_onSignUpRequested);
+    on<AuthSignInRequested>(_onSignInRequested);
+    on<AuthSignOutRequested>(_onSignOutRequested);
+    on<AuthStatusChanged>(_onAuthStatusChanged);
+
+    // Listen to auth state changes
+    _authRepository.authStateChanges.listen((user) {
+      add(AuthStatusChanged(isAuthenticated: user != null));
+    });
+  }
+
+  Future<void> _onSignUpRequested(
+    AuthSignUpRequested event,
+    Emitter<AuthState> emit,
+  ) async {
+    emit(AuthLoading());
+    try {
+      await _authRepository.signUp(
+        email: event.email,
+        password: event.password,
+      );
+    } catch (e) {
+      emit(AuthError(message: e.toString()));
+    }
+  }
+
+  Future<void> _onSignInRequested(
+    AuthSignInRequested event,
+    Emitter<AuthState> emit,
+  ) async {
+    emit(AuthLoading());
+    try {
+      await _authRepository.signIn(
+        email: event.email,
+        password: event.password,
+      );
+    } catch (e) {
+      emit(AuthError(message: e.toString()));
+    }
+  }
+
+  Future<void> _onSignOutRequested(
+    AuthSignOutRequested event,
+    Emitter<AuthState> emit,
+  ) async {
+    await _authRepository.signOut();
+  }
+
+  void _onAuthStatusChanged(AuthStatusChanged event, Emitter<AuthState> emit) {
+    if (event.isAuthenticated) {
+      emit(AuthAuthenticated());
+    } else {
+      emit(AuthUnauthenticated());
+    }
+  }
+}
