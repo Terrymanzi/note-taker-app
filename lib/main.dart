f318f5b6aadc23e7a1aaa4eb17b40c878e1232fb
terrymanzi <m.terry@alustudent.com>
2025-07-06 04:05:29 +0200

    firebase configurations

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'app.dart';
 
 void main() async {
+  //calling
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(const NotesApp());
